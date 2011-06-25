588c0b61d09e065ef9c9c55678551ce0860b74e8
JEONG, MunShik <ruseel@gmail.com>
2011-06-25 17:32:26 +0900

    move files to 'blueprint' direcotry to cope with rails3.1 practices
    - add helper blueprint_ie_fix_stylesheet_link_tag

--- lib/blueprint-rails.rb
+++ lib/blueprint-rails.rb
@@ -1,3 +1,6 @@
+require 'action_controller'
+require 'action_view'
+
 module Blueprint
   module Rails
     if ::Rails.version < "3.1"
@@ -6,4 +9,9 @@ module Blueprint
       require 'blueprint-rails/engine'
     end
   end
+  
+  module Helpers
+    autoload :Helper, 'blueprint-rails/helpers/helper'
+  end
+  ActionController::Base.helper(Blueprint::Helpers::Helper)
 end
